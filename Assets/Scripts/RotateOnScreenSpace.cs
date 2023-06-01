866cef72bb1f8094f0fa02b8020c7815d9a1e4ff
Àngel Llompart <angelllompart@paucasesnovescifp.cat>
2023-06-01 19:23:22 +0200

    Added rotation on x,y and zoom
    Added rotation on x axis, vertical scroll with one finger, rotation on y axis using 3 fingers vertical, added zoom using 2 fingers, it's a scale of the object since the camera moves with the phone itself

--- Assets/Scripts/RotateOnScreenSpace.cs
+++ Assets/Scripts/RotateOnScreenSpace.cs
@@ -21,23 +21,23 @@ public class RotateOnScreenSpace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         if (Touch.activeTouches.Count > 0)
+         if (Touch.activeTouches.Count == 1)
          { 
-             if (Touch.activeTouches[0].phase == TouchPhase.Began) 
+             if(Touch.activeTouches[0].phase == TouchPhase.Moved) 
              { 
-                 //Debug.Log("[Began]" + Touch.activeTouches[0].screenPosition);
-             }
-             else if(Touch.activeTouches[0].phase == TouchPhase.Moved) 
-             { 
-                 //Debug.Log("[Moved position] " + Touch.activeTouches[0].screenPosition);
-                 //Debug.Log("[Moved delta] " + Touch.activeTouches[0].delta);
-                 
+                 transform.Rotate(-Touch.activeTouches[0].delta.y * 360 / Screen.height, 0, 0);
                  transform.Rotate(0, 0, -Touch.activeTouches[0].delta.x * 360 / Screen.width);
              } 
-             else if (Touch.activeTouches[0].phase == TouchPhase.Ended)
-             {
-                 //Debug.Log("[Ended] " + Touch.activeTouches[0].screenPosition);
-             }
+         }
+         
+         else if(Touch.activeTouches.Count == 3)
+         {
+             if(Touch.activeTouches[0].phase == TouchPhase.Moved && 
+                Touch.activeTouches[1].phase == TouchPhase.Moved && 
+                Touch.activeTouches[2].phase == TouchPhase.Moved) 
+             { 
+                 transform.Rotate(0, -Touch.activeTouches[0].delta.y * 360 / Screen.height, 0);
+             } 
          }
     }
 }
--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 public class Zoom : MonoBehaviour
 {
+    private Vector2 pos1;
+    private Vector2 pos2;
+
+    private Vector3 scale = new Vector3(0.1f, 0.1f, 0.1f);
     private void Awake()
     {
         EnhancedTouchSupport.Enable();
@@ -21,20 +26,45 @@ public class Zoom : MonoBehaviour
     {
         if (Touch.activeTouches.Count == 2)
         {
-            Vector2 pos1 = Vector2.zero;
-            Vector2 pos2 = Vector2.zero;
-            if (Touch.activeTouches[1].phase == TouchPhase.Began)
-            {
-                pos2 = Touch.activeTouches[1].delta;
-            }
             if (Touch.activeTouches[0].phase == TouchPhase.Began)
             {
-                pos1 = Touch.activeTouches[0].delta;
+                pos1 = Touch.activeTouches[0].screenPosition;
             }
-            if(Touch.activeTouches[0].phase == TouchPhase.Moved || Touch.activeTouches[1].phase == TouchPhase.Moved) 
-            { 
-                
+            if (Touch.activeTouches[1].phase == TouchPhase.Began)
+            {
+                pos2 = Touch.activeTouches[1].screenPosition;
             }
+
+            float distanceIni = Vector2.Distance(pos1, pos2);
+            
+            if(Touch.activeTouches[0].phase == TouchPhase.Moved || Touch.activeTouches[1].phase == TouchPhase.Moved)
+            {
+                float distance = Vector2.Distance(Touch.activeTouches[0].screenPosition, Touch.activeTouches[1].screenPosition);
+                if (distance > distanceIni)
+                {
+                   ZoomIn();
+                }
+                else if (distance < distanceIni)
+                {
+                   ZoomOut();
+                }
+            }
+        }
+    }
+
+    public void ZoomIn()
+    {
+        if (transform.localScale.y < 2)
+        {
+            transform.localScale += scale;
+        }
+    }
+
+    public void ZoomOut()
+    {
+        if (transform.localScale.y > 0.2f)
+        {
+            transform.localScale -= scale;
         }
     }
 }
